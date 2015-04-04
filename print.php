f051ec3cccd2a3fc57670119d39e4303ddaa116a
Frédéric Bertolus <fred.bertolus@gmail.com>
2015-04-04 11:31:31 +0200

    Add doc

--- print.php
+++ print.php
@@ -5,6 +5,7 @@ $fileToPrint = $_GET['picture'];
 $pdfName = basename($fileToPrint, ".JPG").".pdf";
 
 exec('convert '.$SAVE_DIR.$fileToPrint.' '.$SAVE_DIR.$pdfName);
+//exec('lpr -o PageSize=Custom.10x15cm -o scaling=100 -o fit-to-page -o media=Custom.10x15cm -o position=top'.$SAVE_DIR.$pdfName);
 exec('lpr '.$SAVE_DIR.$pdfName);
 
 ?>
